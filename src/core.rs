d20789793c630851444991fe075feb311a59c820
stevenhuyn <stevenhuyn@gmail.com>
2022-06-04 04:59:21 -0700

    Iter

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,6 @@
-use std::iter;
+use std::{borrow::Cow, iter, mem};
 
+use rand::{distributions::Uniform, prelude::IteratorRandom, thread_rng, Rng, SeedableRng};
 use smaa::SmaaTarget;
 use wgpu::util::DeviceExt;
 use winit::{event::WindowEvent, window::Window};
@@ -9,6 +10,9 @@ use crate::{
     scene::{Scene, Vertex},
     texture::Texture,
 };
+
+const GRID_WIDTH: u32 = 30;
+const TOTAL_CELLS: u32 = GRID_WIDTH * GRID_WIDTH * GRID_WIDTH;
 
 pub struct State {
     surface: wgpu::Surface,
@@ -59,7 +63,7 @@ impl State {
             render_pipeline,
             vertex_buffer,
             index_buffer,
-        ) = State::setup_pipeline(&device, &shader, &config, &camera, &vertices, &indices);
+        ) = State::setup_render_pipeline(&device, &shader, &config, &camera, &vertices, &indices);
         let num_indices = indices.len() as u32;
 
         Self {
@@ -224,7 +228,154 @@ impl State {
         })
     }
 
-    fn setup_pipeline(
+    fn setup_compute_pipeline(
+        config: &wgpu::SurfaceConfiguration,
+        adapter: &wgpu::Adapter,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+    ) {
+        // Compute
+        let compute_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("compute.wgsl"))),
+        });
+
+        // Following Birth/Survive notiation https://conwaylife.com/wiki/Rulestring
+        let birth_list: Vec<u32> = vec![10, 11, 12, 13];
+        let survive_list: Vec<u32> = vec![10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+
+        let birth_list_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Conway Birth List"),
+            contents: bytemuck::cast_slice(&birth_list),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let survive_list_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Conway Survive List"),
+            contents: bytemuck::cast_slice(&survive_list),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let compute_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (birth_list.len() * mem::size_of::<u32>()) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (survive_list.len() * mem::size_of::<u32>()) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 3,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (GRID_WIDTH * GRID_WIDTH * GRID_WIDTH * 4) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 4,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: wgpu::BufferSize::new(
+                                (GRID_WIDTH * GRID_WIDTH * GRID_WIDTH * 4) as _,
+                            ),
+                        },
+                        count: None,
+                    },
+                ],
+                label: None,
+            });
+
+        // Setting up pipeline
+
+        let compute_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("compute"),
+                bind_group_layouts: &[&compute_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: Some("Compute pipeline"),
+            layout: Some(&compute_pipeline_layout),
+            module: &compute_shader,
+            entry_point: "main",
+        });
+
+        // Setting up initial cell data data
+        let mut rng = thread_rng();
+        let mut initial_cell_state: Vec<u32> = (0..TOTAL_CELLS as usize)
+            .map(|_| if rng.gen_bool(0.5) { 0 } else { 1 })
+            .collect();
+
+        // Create two buffers of cell state
+        let mut cell_buffers = Vec::<wgpu::Buffer>::new();
+        let mut cell_bind_groups = Vec::<wgpu::BindGroup>::new();
+        for i in 0..2 {
+            cell_buffers.push(
+                device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some(&format!("Particle Buffer {}", i)),
+                    contents: bytemuck::cast_slice(&initial_cell_state),
+                    usage: wgpu::BufferUsages::VERTEX
+                        | wgpu::BufferUsages::STORAGE
+                        | wgpu::BufferUsages::COPY_DST,
+                }),
+            );
+        }
+
+        // Create 2 bind groups one for each buffer
+        for i in 0..2 {
+            cell_bind_groups.push(device.create_bind_group(&wgpu::BindGroupDescriptor {
+                layout: &compute_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: birth_list_buffer.as_entire_binding(),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: survive_list_buffer.as_entire_binding(),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 2,
+                        resource: cell_buffers[i].as_entire_binding(),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 3,
+                        resource: cell_buffers[(i + 1) % 2].as_entire_binding(), // bind to opposite buffer
+                    },
+                ],
+                label: None,
+            }));
+        }
+    }
+
+    fn setup_render_pipeline(
         device: &wgpu::Device,
         shader: &wgpu::ShaderModule,
         config: &wgpu::SurfaceConfiguration,
